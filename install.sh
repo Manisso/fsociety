4f6281fff8f3ef3b74c389ec98b1f932c1bb34bd
Manisso <manisso@usa.com>
2016-09-01 04:43:35 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "
+███╗   ███╗ █████╗ ███╗   ██╗██╗███████╗███████╗ ██████╗
+████╗ ████║██╔══██╗████╗  ██║██║██╔════╝██╔════╝██╔═══██╗
+██╔████╔██║███████║██╔██╗ ██║██║███████╗███████╗██║   ██║
+██║╚██╔╝██║██╔══██║██║╚██╗██║██║╚════██║╚════██║██║   ██║
+██║ ╚═╝ ██║██║  ██║██║ ╚████║██║███████║███████║╚██████╔╝
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝ 
+▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Ⓜ Ⓐ Ⓝ Ⓘ Ⓢ Ⓢ Ⓞ  ☪ ~
+  █   █  █ █  █ █   ▀▀█ 
+  ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀             
+
+                                                ";
+echo "";
+echo "[✔] You will install fsociety tool in the system [Y/N] :" ;
+read baba
+if [ $baba == "y" ] ; 
+  then
+    echo " "
+  else
+    exit
+fi
+
+echo "[✔] Where Do you want to install the tool? [Ex:/usr/share/doc]:";
+read refdir
+echo "[✔] Checking directories..."
+if [ -d "$refdir/fsociety" ] ;
+then
+echo "[◉] A directory fsociety was found! Do you want to replace it? [Y/n]:" ; 
+read mama
+if [ $mama == "y" ] ; 
+then
+ rm -R "$refdir/fsociety"
+else
+ exit
+fi
+fi
+
+ echo "[✔] Installing ...";
+ echo "";
+ git clone https://github.com/Manisso/fsociety.git $refdir/fsociety;
+ echo "#!/bin/bash 
+ python $refdir/fsociety/fsociety.py" '${1+"$@"}' > fsociety;
+ chmod +x fsociety;
+ sudo cp fsociety /usr/bin/;
+ rm fsociety;
+
+
+if [ -d "$refdir/fsociety" ] ;
+then
+echo "";
+echo "[✔]Tool istalled with success![✔]";
+echo "";
+  echo "[✔]====================================================================[✔]";
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing fsociety !   ✔✔✔ [✔]"; 
+  echo "[✔]====================================================================[✔]";
+  echo "";
+else
+  echo "[✘] Installation faid![✘] ";
+  exit
+fi
