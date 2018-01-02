6d909c5991da1e297ad81aee1574e2a1f06699f2
Aidan Holland <thehappydinoa@gmail.com>
2018-01-02 18:50:16 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,46 +6,46 @@ echo "
 ██╔████╔██║███████║██╔██╗ ██║██║███████╗███████╗██║   ██║
 ██║╚██╔╝██║██╔══██║██║╚██╗██║██║╚════██║╚════██║██║   ██║
 ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║███████║███████║╚██████╔╝
-╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝ 
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝
 ▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Ⓜ Ⓐ Ⓝ Ⓘ Ⓢ Ⓢ Ⓞ  ☪ ~
-  █   █  █ █  █ █   ▀▀█ 
-  ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀             
+  █   █  █ █  █ █   ▀▀█
+  ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀
 
-                                                ";
+";
 
 echo "[✔] Checking directories...";
 if [ -d "/usr/share/doc/fsociety" ] ;
 then
-echo "[◉] A directory fsociety was found! Do you want to replace it? [Y/n]:" ; 
-read mama
-if [ $mama == "y" ] ; 
-then
- rm -R "/usr/share/doc/fsociety"
-else
- exit
-fi
+    echo "[◉] A directory fsociety was found! Do you want to replace it? [Y/n]:" ;
+    read mama
+    if [ $mama == "y" ];
+    then
+        rm -R "/usr/share/doc/fsociety"
+    else
+        exit
+    fi
 fi
 
- echo "[✔] Installing ...";
- echo "";
- git clone https://github.com/Manisso/fsociety.git /usr/share/doc/fsociety;
- echo "#!/bin/bash 
- python /usr/share/doc/fsociety/fsociety.py" '${1+"$@"}' > fsociety;
- chmod +x fsociety;
- sudo cp fsociety /usr/bin/;
- rm fsociety;
+echo "[✔] Installing ...";
+echo "";
+git clone https://github.com/Manisso/fsociety.git /usr/share/doc/fsociety;
+echo "#!/bin/bash
+python /usr/share/doc/fsociety/fsociety.py" '${1+"$@"}' > fsociety;
+chmod +x fsociety;
+sudo cp fsociety /usr/bin/;
+rm fsociety;
 
 
 if [ -d "/usr/share/doc/fsociety" ] ;
 then
-echo "";
-echo "[✔]Tool istalled with success![✔]";
-echo "";
-  echo "[✔]====================================================================[✔]";
-  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing fsociety !   ✔✔✔ [✔]"; 
-  echo "[✔]====================================================================[✔]";
-  echo "";
+    echo "";
+    echo "[✔] Tool istalled with success![✔]";
+    echo "";
+    echo "[✔]====================================================================[✔]";
+    echo "[✔] ✔✔✔  All is done!! You can execute tool by typing fsociety !   ✔✔✔ [✔]";
+    echo "[✔]====================================================================[✔]";
+    echo "";
 else
-  echo "[✘] Installation faid![✘] ";
-  exit
+    echo "[✘] Installation failed![✘] ";
+    exit
 fi
