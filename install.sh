72d0a31e3612e7ab547c760bfff32d16b946f5dd
Aidan Holland <thehappydinoa@gmail.com>
2018-01-03 17:14:59 -0500

    Install.sh Fixed

--- install.sh
+++ install.sh
@@ -13,21 +13,21 @@ echo "
 
 ";
 
-if [ $PREFIX = "/data/data/com.termux/files/usr"]; then
-    INSTALL_DIR=$PREFIX/usr/share/doc/fsociety
-    BIN_DIR=$PREFIX/usr/bin/
+if [ "$PREFIX" = "/data/data/com.termux/files/usr" ]; then
+    INSTALL_DIR="$PREFIX/usr/share/doc/fsociety"
+    BIN_DIR="$PREFIX/usr/bin/"
     pkg install -y git python2
 else
-    INSTALL_DIR=/usr/share/doc/fsociety
-    BIN_DIR=/usr/bin/
+    INSTALL_DIR="/usr/share/doc/fsociety"
+    BIN_DIR="/usr/bin/"
 fi
 
 echo "[✔] Checking directories...";
-if [ -d $INSTALL_DIR ]; then
+if [ -d "$INSTALL_DIR" ]; then
     echo "[◉] A directory fsociety was found! Do you want to replace it? [Y/n]:" ;
     read mama
     if [ $mama == "y" ]; then
-        rm -R $INSTALL_DIR
+        rm -R "$INSTALL_DIR"
     else
         exit
     fi
@@ -35,7 +35,7 @@ fi
 
 echo "[✔] Installing ...";
 echo "";
-git clone https://github.com/Manisso/fsociety $INSTALL_DIR;
+git clone https://github.com/Manisso/fsociety "$INSTALL_DIR";
 echo "#!/bin/bash
 python $INSTALL_DIR/fsociety.py" '${1+"$@"}' > fsociety;
 chmod +x fsociety;
@@ -43,7 +43,7 @@ sudo cp fsociety /usr/bin/;
 rm fsociety;
 
 
-if [ -d $INSTALL_DIR ] ;
+if [ -d "$INSTALL_DIR" ] ;
 then
     echo "";
     echo "[✔] Tool istalled with success![✔]";
